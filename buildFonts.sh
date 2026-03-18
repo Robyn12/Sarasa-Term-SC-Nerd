a382d14cbf90e6cda2885b08088ddef2ad1fc994
Robyn12 <robynsec@proton.me>
2026-03-18 18:55:54 +0200

    Update buildFonts.sh
    Added dependencices and comments

--- buildFonts.sh
+++ buildFonts.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Install dependencies
+sudo pacman -S fontforge p7zip jq
 # Download Sarasa Gothic
 wget -q $(curl 'https://api.github.com/repos/be5invis/Sarasa-Gothic/releases' | jq -r '.[0].assets | map(.browser_download_url) | map(select(test("SarasaTermSC-TTF-[0-9.]+\\.7z"))) | .[0]')
 7zr x Sarasa*.7z
@@ -8,6 +10,7 @@ mv Sarasa*.ttf sarasa
 # Download Font Patcher
 wget -q https://github.com/ryanoasis/nerd-fonts/raw/refs/heads/master/FontPatcher.zip
 unzip FontPatcher.zip
+# patch the fonts
 for file in ./sarasa/*; do
    fontforge -script font-patcher $file
 done
